05d5f5a4b0ae821857d3ade67957e91f932850de
fshstk <fshstk@fshstk.com>
2020-07-19 22:30:17 +0200

    fix bug in post-receive shell script

--- git-post-receive.sh
+++ git-post-receive.sh
@@ -1,5 +1,9 @@
-$BASE_DIR = /home/fshstk_portfolio/web
-$GIT_DIR = /home/fshstk_portfolio/portfolio.git
+#!/bin/bash
+# NOTE: Changes to this script will only propagate on the 2nd push after change.
+# This is because the branch with the new script is checked out by the previous script...
+
+$BASE_DIR="/home/fshstk_portfolio/web"
+$GIT_DIR="/home/fshstk_portfolio/portfolio.git"
 
 printf "Checking out current branch...\n"
 git --work-tree=$BASE_DIR --git-dir=$GIT_DIR checkout -f
